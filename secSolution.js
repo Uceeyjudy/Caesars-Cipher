ba4644a133bc55cc6cb9b99018e6656b91a32f0f
Judith Ezenwosu <ucjudith@gmail.com>
2022-02-17 13:38:36 +0100

    Second solution of the caesars cipher

---
+++ secSolution.js
@@ -0,0 +1,28 @@
+const STR_ROTATION = 13;
+
+function rot13(str) {
+  // Declare an empty string
+  let decodedStr = "";
+
+  // loop over string; for every item in the string
+  for (let i =0; i<str.length; i++){
+    const char = str[i];
+    const charCode =str.charCodeAt(i);
+    // if char is a letter, decode string
+    if (/[A-M]/.test(char)){
+        const decodedChar = charCode + STR_ROTATION;
+        decodedStr += String.fromCharCode(decodedChar);
+    }
+    else if (/[N-Z]/.test(char)){
+        const decodedChar = charCode - STR_ROTATION;
+        decodedStr += String.fromCharCode(decodedChar);
+    }
+    // else add char as it is
+    else{
+      decodedStr += char;
+    }
+  }
+      return decodedStr;
+}
+
+console.log(rot13("SERR PBQR PNZC"));
